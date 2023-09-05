c6f876a21249787f3495ddd0b1cbb510cd1e61c9
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2023-09-05 08:37:27 -0400

    feat: replace static banners for dynamic banners

--- src/components/banner/banner.component.vue
+++ src/components/banner/banner.component.vue
@@ -2,11 +2,49 @@
   <div class="columns is-justify-content-center">
     <div class="banner-container column is-12 is-12-mobile has-text-black">
       <CarouselComponent
+        v-if="banners.length > 0"
         class="banner-img"
-        :carouselItems="banner"
+        :carouselItems="banners"
       ></CarouselComponent>
     </div>
   </div>
 </template>
-<script src="./banner.component.ts" />
+<script>
+import { defineComponent } from 'vue';
+import CarouselComponent from '@/components/carousel/carousel.component.vue';
+import { AxiosService } from '@/core/services/axios.service';
+
+export default defineComponent({
+    name: 'BannerComponent',
+    components: {
+      CarouselComponent,
+    },
+    props: {
+    },
+    data() {
+      return {
+        banners: [],
+      };
+    },
+    async mounted() {
+        this.fetch()
+    },
+    methods: {
+      async fetch() {
+        await AxiosService.endPoint()
+          .get('/public/banners')
+          .then(result => {
+            this.banners = result.data.map((banner) => {
+              let img = `storage/${banner.picture}`
+              return {
+                img,
+                imgPhone: img
+              }
+            })
+          })
+      },
+    }
+});
+
+</script>
 <style lang="scss" src="./banner.component.scss" />
